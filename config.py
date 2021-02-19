9cd72bdf4436da3a5913e2a0ae806be44577c53b
zeka2007 <75169925+zeka2007@users.noreply.github.com>
2021-02-19 22:02:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 setting = {
-    'TOKEN': os.environ.get('BOT_TOKEN')
+    'TOKEN': os.environ.get('BOT_TOKEN'),
     'PREFIX': '.',
     'channels': [777962327357653022,
         778141789500080148,
